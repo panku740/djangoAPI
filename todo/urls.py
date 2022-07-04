703fb68f374556e91dda32ccc47e4e9fed8ffa84
panku740 <pankajkundu9@gmail.com>
2022-07-04 23:29:09 +0530

    Api changes

--- todo/urls.py
+++ todo/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.apiOverview, name='api-overview'),
-    
+	path('', views.apiOverview, name="api-overview"),
+	path('task-list/', views.taskList, name="task-list"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
-from django.http import JsonResponse
 
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TaskSerializer
+from .models import Task
+
+ 
+@api_view(['GET'])
 def apiOverview(request):
-    return JsonResponse("{[API BASE]}",safe=False)
+	api_urls = {
+		'List':'/task-list/',
+		
+		'Create':'/task-create/',
+		'Update':'/task-update/<str:pk>/',
+		'Delete':'/task-delete/<str:pk>/',
+		}
+
+	return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()   
+    serializer = TaskSerializer(tasks,many=True)
+    return Response(serializer.data) 
