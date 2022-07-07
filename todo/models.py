91eda656491c50d4ae4e76319f26df6548c70eb5
panku740 <pankajkundu9@gmail.com>
2022-07-07 22:18:13 +0530

    New learning

--- todo/models.py
+++ todo/models.py
@@ -1,11 +1,20 @@
+from datetime import date
+from time import time
+import uuid
 from django.db import models
+from numpy import true_divide
 
 
 # Create your models here.
 
 class Task(models.Model):
+    uid = models.UUIDField(primary_key= True,editable=False,default=uuid.uuid4())
+    created_at = models.DateField(auto_created=True,default=date.today())
+    updated_at = models.DateField(auto_created=True,default=date.today())
+    
+    class Meta:
+        abstract = True
+
+class Todo(Task):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False,blank=True,null=True)
-
-    def __str__(self):
-        return self.title
--- todo/serializers.py
+++ todo/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
-from .models import Task
+from .models import Todo
 
 class TaskSerializer(serializers.ModelSerializer):
 	class Meta:
-		model = Task
+		model = Todo
 		fields ='__all__'
+
